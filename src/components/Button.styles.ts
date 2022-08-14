328a69997227260fd15e30918a01d5b35e97309a
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-08-14 16:49:28 -0300

    Study styled components with rocktseat

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,22 @@
+import styled,{css} from "styled-components";
+
+
+export type ButtonVariant='primary'|'secondary'|'danger'|'success';
+
+interface ButtonContainerProps{
+    variant:ButtonVariant;
+}
+
+const buttonVariants ={
+    primary:'purple',
+    secondary:'orange',
+    danger:'red',
+    success:'green'
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+width:100px;
+height:40px;
+background-color:${props=>props.theme.primary}
+//${props=> css`background-color:${buttonVariants[props.variant]}`}
+`
