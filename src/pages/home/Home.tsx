ec91e651ae47c189cdd4cd971648baba0683e066
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-08-20 15:30:02 -0300

    Forms React-hook-Form Rocketseat

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,9 @@
 import { Play } from 'phosphor-react'
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
 import {  CountDownContainer,
           FormContainer,
           HomeContainer,
@@ -7,23 +11,66 @@ import {  CountDownContainer,
           Separator,
           StartCountDownButton,
           TaskInput } from './styles';
+import { TypeOf } from 'zod';
+
+
+/* interface NewCycleFormData{
+  task: String;
+  minutesAmount: Number;
+} */
+
+interface 
 
 const Home = () => {
+
+  const newCycleFormValidationSchema = zod.object({
+    task:zod.string().min(1,'Informe a Tarefa'),
+    MinutesAmount:zod.number().min(5).max(60)
+  })
+
+  type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+  const { register, handleSubmit, watch,formState,reset } = useForm<NewCycleFormData>({
+    resolver:zodResolver(newCycleFormValidationSchema),
+    defaultValues:{
+      task: '',
+      MinutesAmount: 5
+    }
+  });
+
+  function handleNewCycle(data:NewCycleFormData){
+    console.log(data);
+    reset();
+  }
+
+  console.log(formState.errors);
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-        <form action="">
+        <form onSubmit={handleSubmit(handleNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="text" id='task' list='task-sugestions' placeholder='Dê um nome para seu projeto'/>
+          <TaskInput type="text" id='task' 
+           list='task-sugestions'
+           placeholder='Dê um nome para seu projeto'
+           {...register('task')}
+           />
           <datalist id='task-sugestions'>
             <option value="Projeto 1"/>
             <option value="Projeto 2"/>
           </datalist>
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number"  id='minutesAmount' placeholder='00'
+          <MinutesAmountInput
+           type="number"
+           id='minutesAmount'
+           placeholder='00'
             step={5}
             min={5}
-            max={60}
+            //max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos</span>
         </FormContainer>
@@ -38,7 +85,7 @@ const Home = () => {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton disabled type='submit'>
+        <StartCountDownButton  type='submit' disabled={isSubmitDisabled}>
           <Play size={24}/>
           Começar</StartCountDownButton>
         </form>
